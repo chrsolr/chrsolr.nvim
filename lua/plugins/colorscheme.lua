193485edf1a5d2b1319508a7ad46f4d089ec5377
iwchrsolr <christian@innowatts.com>
2023-07-05 00:16:39 -0500

    Update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-   {"catppuccin/nvim", name = "catppuccin", priority = 1000 },
-   {
-      "LazyVim/LazyVim",
-      opts = {
-         colorscheme = "catppuccin-mocha"
-      }
-   }
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+
+  {
+    'folke/tokyonight.nvim',
+    lazy = true,
+    priority = 1000,
+    opts = { style = 'storm' },
+  },
+
+  { 'LazyVim/LazyVim', opts = { colorscheme = 'catppuccin-mocha' } },
 }
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,15 +1,3 @@
 return {
-
   { 'nvim-tree/nvim-web-devicons', lazy = true },
-  
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-  {
-    'folke/tokyonight.nvim',
-    lazy = true,
-    priority = 1000,
-    opts = {
-      style = 'storm',
-    },
-  },
 }
