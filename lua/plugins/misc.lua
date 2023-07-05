3c4b66b1646122ec1b155f7c223fc9cd1724592e
iwchrsolr <christian@innowatts.com>
2023-07-04 23:21:42 -0500

    Updated

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,15 @@
+return {
+
+  { 'nvim-tree/nvim-web-devicons', lazy = true },
+  
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  {
+    'folke/tokyonight.nvim',
+    lazy = true,
+    priority = 1000,
+    opts = {
+      style = 'storm',
+    },
+  },
+}
