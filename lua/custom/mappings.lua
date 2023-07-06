519520e5ca4525d9c7fec38743783d9078286830
iwchrsolr <christian@innowatts.com>
2023-07-06 09:33:29 -0500

    updated

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,6 +8,12 @@ M.general = {
     ["<C-u>"] = { "<C-u>zz", "Scroll up and center", opts = { nowait = true } },
     ["<C-j>"] = { "}", "Scroll down to next blankline ", opts = { nowait = true } },
     ["<C-k>"] = { "{", "Scroll up to next blankline ", opts = { nowait = true } },
+    ["<S-h>"] = { "<C-w>h", "Window left" },
+    ["<S-l>"] = { "<C-w>l", "Window right" },
+    ["<S-j>"] = { "<C-w>j", "Window down" },
+    ["<S-k>"] = { "<C-w>k", "Window up" },
+    ["<leader>|"] = { "<cmd> vsp <CR>", "Vertical Split", opts = { nowait = true } },
+    ["<leader>-"] = { "<cmd> sp <CR>", "Horizantal Split", olpts = { nowait = true } },
   },
 
   v = {
