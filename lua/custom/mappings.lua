8dcd80b045fd93dfb8b4b9d5645dff07e85b0ac7
iwchrsolr <christian@innowatts.com>
2023-07-05 23:45:13 -0500

    add keymaps

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,7 +4,16 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-d>"] = { "<C-d>zz", "Scroll down and center", opts = { nowait = true } },
+    ["<C-u>"] = { "<C-u>zz", "Scroll up and center", opts = { nowait = true } },
+    ["<C-j>"] = { "}", "Scroll down to next blankline ", opts = { nowait = true } },
+    ["<C-k>"] = { "{", "Scroll up to next blankline ", opts = { nowait = true } },
   },
+
+  v = {
+    ["<C-j>"] = { "}", "Scroll down to next blankline ", opts = { nowait = true } },
+    ["<C-k>"] = { "{", "Scroll up to next blankline ", opts = { nowait = true } },
+  }
 }
 
 -- more keybinds!
