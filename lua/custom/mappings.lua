89476e5051922c8820f92cdfa59e825958cb4671
iwchrsolr <christian@innowatts.com>
2023-07-10 11:37:04 -0500

    updated

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,14 +5,22 @@ M.general = {
   n = {
     ["<C-d>"] = { "<C-d>zz", "Scroll down and center", opts = { nowait = true } },
     ["<C-u>"] = { "<C-u>zz", "Scroll up and center", opts = { nowait = true } },
+
     ["<C-j>"] = { "}", "Scroll down to next blankline ", opts = { nowait = true } },
     ["<C-k>"] = { "{", "Scroll up to next blankline ", opts = { nowait = true } },
-    ["<S-h>"] = { "<C-w>h", "Window left" },
-    ["<S-l>"] = { "<C-w>l", "Window right" },
+    
     ["<leader>|"] = { "<cmd> vsp <CR>", "Vertical Split", opts = { nowait = true } },
     ["<leader>-"] = { "<cmd> sp <CR>", "Horizantal Split", opts = { nowait = true } },
+
     ["<leader>tt"] = { "<cmd> TroubleToggle <CR>", "Trouble Toggle", opts = { nowait = true} },
     ["<leader>gg"] = { "<cmd> LazyGit <CR>", "Open LazyGit", opts = { nowait = true} },
+
+    ["<leader><C-f>"] = {
+      function()
+        vim.lsp.buf.format { async = true }
+      end,
+      "LSP formatting",
+    },
   },
 
   v = {
