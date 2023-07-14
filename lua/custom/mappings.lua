663edba86c4ef935f61c9d5fb74489237b8b9ad5
iwchrsolr <christian@innowatts.com>
2023-07-14 16:13:29 -0500

    update

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,18 +3,24 @@ local M = {}
 
 M.general = {
   n = {
+    -- Motions
     ["<C-d>"] = { "<C-d>zz", "Scroll down and center", opts = { nowait = true } },
     ["<C-u>"] = { "<C-u>zz", "Scroll up and center", opts = { nowait = true } },
-
     ["<C-j>"] = { "}", "Scroll down to next blankline ", opts = { nowait = true } },
     ["<C-k>"] = { "{", "Scroll up to next blankline ", opts = { nowait = true } },
-    
+
+
+    -- Windows
     ["<leader>|"] = { "<cmd> vsp <CR>", "Vertical Split", opts = { nowait = true } },
     ["<leader>-"] = { "<cmd> sp <CR>", "Horizantal Split", opts = { nowait = true } },
 
+
+    -- Commands
     ["<leader>ct"] = { "<cmd> TroubleToggle <CR>", "Trouble Toggle", opts = { nowait = true} },
     ["<leader>gg"] = { "<cmd> LazyGit <CR>", "Open LazyGit", opts = { nowait = true} },
 
+
+    -- Formats
     ["<leader>cf"] = {
       function()
         vim.lsp.buf.format { async = true }
@@ -23,6 +29,9 @@ M.general = {
       "LSP & Eslint formatting",
     },
 
+
+    -- Spell
+    ["<leader>zg"] = { "zg", "Add word to dictionary", opts = { nowait = true } },
     ["<leader>z-"] = {
       function()
         vim.opt_local.spell = not(vim.opt_local.spell:get())
@@ -30,7 +39,6 @@ M.general = {
       end,
       "Toggle spell check"
     },
-
   },
 
   v = {
