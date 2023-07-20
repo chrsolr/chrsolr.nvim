479713d3be56090ad9d11db115d0b9f966cdd3cb
iwchrsolr <christian@innowatts.com>
2023-07-20 10:17:27 -0500

    updated

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyonight",
-  theme_toggle = { "tokyonight", "one_light" },
+  theme = "chadracula",
+  theme_toggle = { "chadracula", "chadracula" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
