d336df4a6ca1302fb13eb0e35c61b84cf7e575d2
chrsolr <chr.solr@gmail.com>
2023-10-21 12:04:31 -0500

    updated

--- init.lua
+++ init.lua
@@ -100,7 +100,7 @@ require('lazy').setup({
 
   {
     "folke/tokyonight.nvim",
-    priority = 1000,
+    priority = 1002,
     config = function()
       require("tokyonight").setup {
         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
@@ -113,7 +113,7 @@ require('lazy').setup({
 
   {
     "catppuccin/nvim",
-    priority = 1001,
+    priority = 1000,
     config = function()
       require("catppuccin").setup {
         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
@@ -127,7 +127,7 @@ require('lazy').setup({
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    priority = 1002,
+    priority = 1001,
     config = function()
       require("rose-pine").setup {
         --- @usage 'auto'|'main'|'moon'|'dawn'
@@ -147,7 +147,9 @@ require('lazy').setup({
       options = {
         icons_enabled = false,
         -- theme = 'onedark',
-        theme = 'tokyonight',
+        -- theme = 'tokyonight',
+        -- theme = 'rose-pine',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
