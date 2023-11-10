c62938d64a08dfacbce596852194b059e8fdbc2d
chrsolr <chr.solr@gmail.com>
2023-11-10 12:02:14 -0600

    updated

--- init.lua
+++ init.lua
@@ -252,7 +252,7 @@ require('telescope').setup {
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'c_sharp' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
