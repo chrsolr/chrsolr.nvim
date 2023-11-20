56942a4ba18ad66f8d0c3b99ecee8c91ec8a7160
chrsolr <chr.solr@gmail.com>
2023-11-20 09:01:36 -0600

    updated

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -9,18 +9,18 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { { "biome", "prettier" } },
-        typescript = { { "biome", "prettier" } },
-        javascriptreact = { { "biome", "prettier" } },
-        typescriptreact = { { "biome", "prettier" } },
+        javascript = { { "prettier" } },
+        typescript = { { "prettier" } },
+        javascriptreact = { { "prettier" } },
+        typescriptreact = { { "prettier" } },
         css = { { "prettier" } },
-        tailwindcss = { { "biome", "prettier" } },
-        html = { { "biome", "prettier" } },
-        json = { { "biome", "prettier" } },
-        yaml = { { "biome", "prettier" } },
-        markdown = { { "biome", "prettier" } },
+        tailwindcss = { { "prettier" } },
+        html = { { "prettier" } },
+        json = { { "prettier" } },
+        yaml = { { "prettier" } },
+        markdown = { { "prettier" } },
         lua = { { "stylua" } },
-        csharp = { { "biome", "csharpier" } },
+        csharp = { { "csharpier" } },
       },
       format_on_save = {
         lsp_fallback = true,
